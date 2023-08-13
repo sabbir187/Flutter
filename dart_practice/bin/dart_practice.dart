32aa0774e972bad5cdeaf2a9e440f72157b7366e
md sabbir <mdsabbirahmed187@gmail.com>
2023-08-13 18:40:21 +0600

    data type of dart

--- dart_practice/bin/dart_practice.dart
+++ dart_practice/bin/dart_practice.dart
@@ -1,3 +1,16 @@
 void main(){
   print('hello dart');
+  var intNumber=10;
+  var doubleNumber=32.454;
+  print(intNumber);
+  print(doubleNumber);
+  var x=34;
+  var y=8.00000;
+  var z=x%y;
+  print(z);
+  var myname="My Name is Sabbir";// string in double quotion.
+  var myName='My name is sabbir';//single quotion.
+  print(myname);
+  print(myName);
+
 }
